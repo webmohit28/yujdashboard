cd39ec4cf97be123159ca48634a3234cd29b5530
Mohit <mohit@Yujadmin-pro16s-MacBook-Pro.local>
2021-11-19 10:36:09 +0530

    created basic layout in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainContainerComponent } from './main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule
